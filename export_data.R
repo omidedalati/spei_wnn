35d4718f09e38b39cd845878f333998f5b416a0b
omid edalati <135520801+omidedalati@users.noreply.github.com>
2025-08-12 00:34:26 +0330

    export_data.R

--- export_data.R
+++ export_data.R
@@ -1,17 +1,13 @@
-library(zoo)  # برای ساخت توالی زمانی
+library(zoo)  
 
-# فرض: ts_data و res موجود هستند
-# 1) داده اصلی
 dates_main <- as.yearmon(time(ts_data))
 values_main <- as.numeric(ts_data)
 
-# 2) داده پیش‌بینی
 fc <- as.numeric(res$Finalforecast)
 dates_fc <- seq(from = max(dates_main) + 1/frequency(ts_data),
                 by   = 1/frequency(ts_data),
                 length.out = length(fc))
 
-# 3) ترکیب در یک دیتافریم
 df_all <- data.frame(
   Date  = c(dates_main, dates_fc),
   Value = c(values_main, fc),
@@ -19,7 +15,7 @@ df_all <- data.frame(
             rep("Forecast", length(fc)))
 )
 
-# نمایش
-print(head(df_all, 15))   # اولین چند ردیف
-print(tail(df_all, 15))   # آخرین چند ردیف
+
+print(head(df_all, 15))    
+print(tail(df_all, 15))   
 write.csv(df_all, file = "dataset_pet.csv", row.names = FALSE)
